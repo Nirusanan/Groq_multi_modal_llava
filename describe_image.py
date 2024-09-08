08bc2480c4fe994b5d43329142097601e2e2f262
Nirusanan Niru <47416743+Nirusanan@users.noreply.github.com>
2024-09-08 18:33:33 +0530

    multi-modal_llava_llama3.1

---
+++ describe_image.py
@@ -0,0 +1,41 @@
+# 1. Imports and API setup
+from groq import Groq
+import base64
+
+client = Groq()
+llava_model = 'llava-v1.5-7b-4096-preview'
+llama31_model = 'llama-3.1-70b-versatile'
+
+# 2. Image encoding
+image_path = 'y3.jpg'
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+base64_image = encode_image(image_path)
+
+# 3. Image to text function
+def image_to_text(client, model, base64_image, prompt):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": prompt},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}",
+                        },
+                    },
+                ],
+            }
+        ],
+        model=model
+    )
+
+    return chat_completion.choices[0].message.content
+
+prompt = "Describe this image"
+print(image_to_text(client, llava_model, base64_image, prompt))
+
